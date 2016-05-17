9f05ed63e51429bb5f0d70dfcef507b8e780a923
deliverea <hello@deliverea.com>
2016-05-17 17:22:40 +0200

    Fix status trackings response.

--- src/Deliverea/Response/GetShipmentResponse.php
+++ src/Deliverea/Response/GetShipmentResponse.php
@@ -31,18 +31,27 @@ class GetShipmentResponse extends AbstractResponse
         $to = $this->createAddress('to', $response->to_data);
         $from = $this->createAddress('from', $response->from_data);
 
-        $events = [];
-        foreach ($response->tracking_events->tracking_history as $event) {
-            $events[] = $this->createTrackingEvent($event);
+        $serviceLevelData = null;
+        $trackingEvents = null;
+
+        if (!empty($response->tracking_events)) {
+            $events = [];
+            foreach ($response->tracking_events->tracking_history as $event) {
+                $events[] = $this->createTrackingEvent($event);
+            }
+
+
+            $trackingEvents = new TrackingEvents($this->createTrackingEvent($response->tracking_events), $events);
         }
 
-        $trackingEvents = new TrackingEvents($this->createTrackingEvent($response->tracking_events), $events);
-        $serviceLevelData = new SLAData(
-            $response->sla_data->tracking_start_date,
-            $response->sla_data->tracking_delivered_date,
-            $response->sla_data->tracking_current_code,
-            $response->sla_data->hours_elapsed
-        );
+        if (!empty($response->sla_data)) {
+            $serviceLevelData = new SLAData(
+                $response->sla_data->tracking_start_date,
+                $response->sla_data->tracking_delivered_date,
+                $response->sla_data->tracking_current_code,
+                $response->sla_data->hours_elapsed
+            );
+        };
 
         $this->shipment = new DetailedShipment($shipment, $from, $to, $trackingEvents, $serviceLevelData);
 
